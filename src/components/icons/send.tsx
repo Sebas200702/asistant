ddd049bda2ba555ac5f7c5dbde844091f0e9502c
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-12 23:06:52 -0500

    Add SendIcon component: implement SendIcon for sending functionality

---
+++ src/components/icons/send.tsx
@@ -0,0 +1,18 @@
+import type { IconProps } from '../../types'
+
+export const SendIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M12 5v14M18 11l-6-6M6 11l6-6" />
+    </svg>
+  )
+}
