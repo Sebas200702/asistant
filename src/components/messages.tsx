9eb08b50aaa3a18dba7f254897f0873799b6f073
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-12 23:06:35 -0500

    Add MessagesComponent: implement MessagesComponent to display chat messages with a default prompt when empty

---
+++ src/components/messages.tsx
@@ -0,0 +1,34 @@
+import { MesssageBox } from '@components/message-box'
+import { useChatStore } from '@store/chat-store'
+import { useEffect } from 'react'
+
+export const MessagesComponent = () => {
+  const { messages } = useChatStore()
+  useEffect(() => {
+    console.log(messages.map(message => message.content))
+  }, [messages])
+
+  return (
+    <ul className="custom-scrollbar flex h-full max-h-180 w-full flex-col gap-6 overflow-y-auto p-4 px-80">
+      {messages.length === 0 && (
+        <div className="flex h-full w-full -translate-y-10 flex-col items-center justify-center gap-3 text-gray-600">
+          <h2 className="text-3xl font-bold text-gray-900">
+            Hola soy [Nombre]
+          </h2>
+          <p className="text-sm text-gray-600">En que puedo ayudarte hoy?</p>
+        </div>
+      )}
+      {messages?.map((message, index) => (
+        <MesssageBox
+          key={message.id}
+          id={message.id}
+          content={message.content}
+          role={message.role}
+          timestamp={message.timestamp}
+          avatarUrl={message.avatarUrl}
+          isLastMessage={index === messages.length - 1}
+        />
+      ))}
+    </ul>
+  )
+}
