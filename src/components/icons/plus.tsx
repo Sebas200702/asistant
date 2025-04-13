d4551413be749b064fa45149e7e652e9f0a857c8
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-12 23:06:45 -0500

    Add PlusIcon component: implement PlusIcon for adding functionality

---
+++ src/components/icons/plus.tsx
@@ -0,0 +1,18 @@
+import type { IconProps } from '../../types'
+
+export const PlusIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <path d="M12 5v14m-7-7h14"></path>
+    </svg>
+  )
+}
