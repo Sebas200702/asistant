a9b23c073f7728dffcad5a500d4f97dcc9790dac
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-20 16:02:14 -0500

    Refactor POST handler to include dynamic context in chat prompt generation

--- src/pages/api/responseUserMessage.ts
+++ src/pages/api/responseUserMessage.ts
@@ -1,12 +1,27 @@
 import { chat } from '@libs/gemini'
+
+import { getDynamicContext } from '@utils/get-dinamic-context'
 import type { APIRoute } from 'astro'
 import type { Message } from '../../types'
 
 export const POST: APIRoute = async ({ request }) => {
   const { message }: { message: Message } = await request.json()
   const { content } = message
-  const response = await chat.sendMessageStream(content)
 
+  const contextResults = await getDynamicContext(content)
+  const bestContexts = contextResults.slice(0, 3).map((c) => c.content)
+  const fullPrompt = `
+     USA TODA LA INFORMACION A CONTINUACION PARA RESPONDER LA PREGUNTA DEL USUARIO.
+      ${bestContexts.join('\n\n------\n')}
+
+
+      PREGUNTA DEL USUARIO:
+      ${content}
+    `
+
+  const response = await chat.sendMessageStream(fullPrompt)
+
+  console.log(bestContexts)
   if (
     !response ||
     typeof response.stream[Symbol.asyncIterator] !== 'function'
