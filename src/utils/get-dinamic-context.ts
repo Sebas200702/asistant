d8bafaac15d067ba27a4a10c6eb5223d41dbdfa6
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-20 16:02:29 -0500

    Add getDynamicContext utility function for fetching and scoring dynamic context based on user input

---
+++ src/utils/get-dinamic-context.ts
@@ -0,0 +1,30 @@
+import { supabase } from '@libs/supabase'
+import { calculateMatchScore } from '@utils/calculate-match-score'
+
+export const getDynamicContext = async (
+  userInput: string,
+  topK: number = 3
+) => {
+  const tokens = userInput
+    .toLowerCase()
+    .split(/[\s,.;:!?]+/)
+    .filter((t) => t.length > 3)
+
+  const { data, error } = await supabase
+    .from('University_data')
+    .select('id, content, keywords')
+    .or(tokens.map((t) => `keywords.cs.{${t}}`).join(','))
+    .limit(topK)
+
+  if (error) {
+    console.error('Error fetching data:', error)
+  }
+  return (
+    data
+      ?.map((item) => ({
+        score: calculateMatchScore(tokens, item.keywords),
+        ...item,
+      }))
+      .sort((a, b) => b.score - a.score) || []
+  )
+}
