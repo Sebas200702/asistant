ff20e9782105f91f196908140b9d6e09834370f0
Sebas200702 <sebastorregroza6@gmail.com>
2025-05-26 00:21:23 -0500

    Refactor astro.config.mjs for improved readability and consistency in formatting. Adjusted indentation for better alignment of configuration properties.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,10 @@ import auth from 'auth-astro';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  output: 'server',
-  integrations: [react(), auth()],
-  adapter: vercel(),
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    output: 'server',
+    integrations: [react(), auth()],
+    adapter: vercel(),
 })
