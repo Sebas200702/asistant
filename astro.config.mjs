9e17e434d60f25563e895481f4c3d14b80e0e288
Sebas200702 <sebastorregroza6@gmail.com>
2025-05-25 22:25:32 -0500

    Add auth-astro integration to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+import auth from 'auth-astro';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   output: 'server',
-  integrations: [react()],
+  integrations: [react(), auth()],
   adapter: vercel(),
 })
